13bd406065120233b0de9ba0cf94f6a2a641002a
sasha <akamenjasevic@itekako.com>
2021-12-20 15:00:59 +0100

    update websecurity configuration

--- EbfEmployees/src/main/java/com/itekako/EbfEmployees/configurations/WebSecurityConfiguration.java
+++ EbfEmployees/src/main/java/com/itekako/EbfEmployees/configurations/WebSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.itekako.EbfEmployees.configurations;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,14 +11,16 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
+@Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().antMatchers("/**").permitAll()
+                .and().csrf().disable().cors();
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource(){
+    public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
                 Arrays.asList("http://localhost:4200/*",
