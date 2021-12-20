199968bc5eeca52f1979a06d68f1c6f8a306f73c
sasha <akamenjasevic@itekako.com>
2021-12-20 13:02:58 +0100

    initial commit

---
+++ EbfEmployees/src/main/java/com/itekako/EbfEmployees/EbfEmployeesApplication.java
@@ -0,0 +1,13 @@
+package com.itekako.EbfEmployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbfEmployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbfEmployeesApplication.class, args);
+	}
+
+}
---
+++ EbfEmployees/src/test/java/com/itekako/EbfEmployees/EbfEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itekako.EbfEmployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbfEmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
