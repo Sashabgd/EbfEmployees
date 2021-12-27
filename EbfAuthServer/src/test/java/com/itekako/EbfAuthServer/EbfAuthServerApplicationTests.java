8fe358e826cd2b62f5754f91ddef56ce85801d83
sasha <akamenjasevic@itekako.com>
2021-12-27 15:16:08 +0100

    initial commit for auth server

---
+++ EbfAuthServer/src/test/java/com/itekako/EbfAuthServer/EbfAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itekako.EbfAuthServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbfAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
