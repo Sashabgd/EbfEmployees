c80b5d8a1b0e936f58beaf270d99146160d2c122
sasha <akamenjasevic@itekako.com>
2021-12-29 20:24:44 +0100

    companies avarage salary table

--- EbfEmployeesFront/src/app/components/reports/reports.component.ts
+++ EbfEmployeesFront/src/app/components/reports/reports.component.ts
@@ -1,4 +1,11 @@
+import { CompanySalaryModel } from './../../models/company-salary.model';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { CompanyModel } from 'src/app/models/company.model';
+import { PageModel } from 'src/app/models/page.model';
 
 @Component({
   selector: 'app-reports',
@@ -7,9 +14,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  public tableData!: PageModel<CompanySalaryModel>;
+  displayedColumns: string[] = ['id', 'name','avgSalary', 'delete'];
+
+  constructor(private httpClient: HttpClient, private snackBar: MatSnackBar, private router: Router, private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.loadCompanies();
   }
 
+  public deleteCompany(company: CompanyModel): void {
+    this.httpClient.delete(`/api/companies/${company.id}`).subscribe({
+      next: () => {
+        this.loadCompanies();
+      },
+      error: () => {
+        this.snackBar.open("Error deleting company", "OK", { duration: 2000 });
+      }
+    });
+  }
+
+  private loadCompanies(): void {
+    this.httpClient.get<PageModel<CompanySalaryModel>>('/api/companies/avg-salaries').subscribe(r => {
+      this.tableData = r;
+    })
+  }
+
+  public openCompanyDetail(company: CompanyModel): void {
+    this.router.navigate([`/company/${company.id}`])
+  }
 }
