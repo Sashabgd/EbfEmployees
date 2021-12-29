f3f6a8a1b2c0e27b8a2a88e4ad5e97219b7b333d
sasha <akamenjasevic@itekako.com>
2021-12-29 20:32:07 +0100

    employee list table

--- EbfEmployeesFront/src/app/components/employees/employees.component.ts
+++ EbfEmployeesFront/src/app/components/employees/employees.component.ts
@@ -1,4 +1,9 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { PageModel } from './../../models/page.model';
 import { Component, OnInit } from '@angular/core';
+import { EmployeeModel } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-employees',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesComponent implements OnInit {
 
-  constructor() { }
+  public employees!: PageModel<EmployeeModel>;
+  displayedColumns: string[] = ['id', 'name', 'surname', 'email', 'address', 'company', 'salary', 'delete'];
+
+  constructor(
+    private route: Router,
+    private httpClient: HttpClient,
+    private snackbar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+    this.loadEmployees();
   }
 
+  public deleteEmployee(employee: EmployeeModel): void {
+    this.httpClient.delete(`/api/employees/${employee.id}`)
+      .subscribe({
+        next: () => {
+          this.loadEmployees();
+        },
+        error: (e) => {
+          this.snackbar.open(e.message, "OK", { duration: 2000 });
+        }
+      })
+  }
+
+  public openEmployeeDetail(employee: EmployeeModel): void {
+    this.route.navigate([`/employee/${employee.id}`]);
+  }
+
+  private loadEmployees() {
+    this.httpClient.get<PageModel<EmployeeModel>>(`/api/employees`)
+      .subscribe({
+        next: (employees) => {
+          this.employees = employees;
+        },
+        error: (e) => {
+          this.snackbar.open(e.message, "OK", { duration: 2000 });
+        }
+      })
+  }
 }
