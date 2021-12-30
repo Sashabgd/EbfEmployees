8ccdb11397042ea98b712cf6214247a349b64848
sasha <akamenjasevic@itekako.com>
2021-12-30 17:34:18 +0100

    update employee

--- EbfEmployeesFront/src/app/components/employee-details/employee-details.component.ts
+++ EbfEmployeesFront/src/app/components/employee-details/employee-details.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { EmployeeModel } from './../../models/employee.model';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-details',
@@ -13,11 +14,13 @@ export class EmployeeDetailsComponent implements OnInit {
 
   public employee!: EmployeeModel;
   public id!: number;
+  public employeeForm!: FormGroup;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private httpClient: HttpClient,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private fb: FormBuilder
   ) {
     this.activatedRoute.params.subscribe(r => {
       if (r["id"]) {
@@ -28,17 +31,57 @@ export class EmployeeDetailsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+
+    this.employeeForm = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', Validators.email],
+      address: ['', Validators.required],
+      salary: ['', Validators.required]
+    });
+
+    this.loadEmployee();
+  }
+
+  public loadEmployee() {
     if (this.id) {
       this.httpClient.get<EmployeeModel>(`/api/employees/${this.id}`)
         .subscribe({
           next: (e) => {
             this.employee = e;
+            this.employeeForm.controls['name'].setValue(e.name);
+            this.employeeForm.controls['surname'].setValue(e.surname);
+            this.employeeForm.controls['email'].setValue(e.email);
+            this.employeeForm.controls['address'].setValue(e.address);
+            this.employeeForm.controls['salary'].setValue(e.salary);
           },
           error: (e) => {
             this.snackBar.open(e.message, "OK", { duration: 2000 });
           }
         })
     }
+  }
+
+  public updateEmployee(): void {
+    if (this.employeeForm.invalid) {
+      this.snackBar.open("Form is invalid", "OK", { duration: 2000 });
+      return;
+    }
+    this.httpClient.put(`/api/employees/${this.id}`, {
+      name: this.employeeForm.controls['name'].value,
+      surname: this.employeeForm.controls['surname'].value,
+      email: this.employeeForm.controls['email'].value,
+      address: this.employeeForm.controls['address'].value,
+      salary: this.employeeForm.controls['salary'].value,
+      companyId: this.employee.company.id,
+    }).subscribe({
+      next: () => {
+        this.loadEmployee();
+      },
+      error: (e) => {
+        this.snackBar.open(e.message, "OK", { duration: 2000 });
+      }
+    })
   }
 }
 
